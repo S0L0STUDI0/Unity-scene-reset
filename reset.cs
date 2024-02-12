46f997268646fdae8d2f1668e0ab4d66a43ce9fe
#SoloStudio <147620785+S0L0STUDI0@users.noreply.github.com>
2024-02-12 17:15:40 +0700

    Update reset.cs

--- reset.cs
+++ reset.cs
@@ -1,3 +1,10 @@
+//   _  _    _____       _       _____ _             _ _       
+// _| || |_ / ____|     | |     / ____| |           | (_)      
+//|_  __  _| (___   ___ | | ___| (___ | |_ _   _  __| |_  ___  
+// _| || |_ \___ \ / _ \| |/ _ \\___ \| __| | | |/ _` | |/ _ \ 
+//|_  __  _|____) | (_) | | (_) |___) | |_| |_| | (_| | | (_) |
+//  |_||_| |_____/ \___/|_|\___/_____/ \__|\__,_|\__,_|_|\___/ 
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -20,3 +27,5 @@ public class Reset : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex);
     }
 }
+
+//SoloStudio-2024
